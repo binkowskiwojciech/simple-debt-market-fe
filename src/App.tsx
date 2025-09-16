fbf3a015be58ae45d1acc5b7ffcb71a84cfb00df
Wojciech Bińkowski <wojciech.binkowski@4chain.studio>
2025-09-16 17:21:34 +0200

    chore: implement linter and prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <>
       <h1>Simple debt market</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
